c55221a3f436bfa983a34f5e2ccabbb339cde9fb
fredcallaway <fredc@llaway.com>
2023-04-22 14:56:13 -0400

    improve fetch_data

--- bin/fetch_data.py
+++ bin/fetch_data.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
 import os
+import subprocess
 import pandas as pd
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import hashlib
-from psiturk.models import Participant
 
-def hash_id(worker_id):
-    return 'md5:' + hashlib.md5(worker_id.encode()).hexdigest()
+# set environment parameters so that we use the remote database
 
-def write_csv_from_db(filename, version):
+def get_database():
+    if os.path.isfile('.database_url'):
+        with open('.database_url') as f:
+            return f.read()
+    else:
+        cmd = "heroku config:get DATABASE_URL"
+        url = subprocess.check_output(cmd, shell=True).strip().decode('ascii')
+        with open('.database_url', 'w') as f:
+            f.write(url)
+            return url
+
+env = os.environ
+env["PORT"] = ""
+env["ON_CLOUD"] = "1"
+env["DATABASE_URL"] = get_database()
+
+from psiturk.models import Participant  # must be imported after setting params
+
+
+class Anonymizer(object):
+    def __init__(self):
+        self.mapping = {}
+
+    def __call__(self, uniqueid):
+        worker_id, assignment_id = uniqueid.split(':')
+        if worker_id not in self.mapping:
+            self.mapping[worker_id] = 'w' + hashlib.md5(worker_id.encode()).hexdigest()[:7]
+        return self.mapping[worker_id]
+
+
+def main(version, debug):
     ps = Participant.query.filter(Participant.codeversion == version).all()
-    # We filter out any debug trials.
-    ps = [p for p in ps if 'debug' not in p.uniqueid]
-    # HACK we don't filter out for completion status.
+    if not debug:
+        ps = [p for p in ps if 'debug' not in p.uniqueid]
+    print(len(ps), 'participants')
+
+    anonymize = Anonymizer()
+
+    # Note: we don't filter by completion status.
 
     def qdata(p):
         '''
@@ -34,38 +67,51 @@ def write_csv_from_db(filename, version):
         "questiondata": qdata,
     }
 
-    data = []
-    for p in ps:
-        try:
-            data.append(contents[filename](p))
-        except:
-            import traceback
-            traceback.print_exc()
-    data = "".join(data)
-
-    # write out the data file
-    dest = os.path.join('data/human_raw', version, "{}.csv".format(os.path.splitext(filename)[0]))
-    if not os.path.exists(os.path.dirname(dest)):
-        os.makedirs(os.path.dirname(dest))
-    with open(dest, "w") as fh:
-        fh.write(data)
-
-    # Anonymize PIDs
-    df = pd.read_csv(dest, header=None)
-    df[0] = df[0].map(hash_id)
-    df.to_csv(dest, header=None, index=False)
-
-def main(version):
     for filename in ["trialdata", "eventdata", "questiondata"]:
-        write_csv_from_db(filename, version)
+        data = []
+        for p in ps:
+            try:
+                data.append(contents[filename](p))
+            except:
+                import traceback
+                traceback.print_exc()
+        data = "".join(data)
+
+        # write out the data file
+
+        dest = os.path.join('data/human_raw', version, "{}.csv".format(os.path.splitext(filename)[0]))
+        if not os.path.exists(os.path.dirname(dest)):
+            os.makedirs(os.path.dirname(dest))
+        with open(dest, "w") as fh:
+            fh.write(data)
+
+        # Anonymize PIDs
+        df = pd.read_csv(dest, header=None)
+        df[0] = df[0].map(anonymize)
+        df.to_csv(dest, header=None, index=False)
+
+    fp = os.path.join('data/human_raw', version, "identifiers.csv")
+    pd.Series(anonymize.mapping, name='wid').to_csv('.csv', index_label='workerid')
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument(
         "version",
+        nargs="?",
         help=("Experiment version. This corresponds to the experiment_code_version "
               "parameter in the psiTurk config.txt file that was used when the "
               "data was collected."))
+    parser.add_argument("--debug", help="Keep debug participants", action="store_true")
+
     args = parser.parse_args()
-    main(args.version)
+    version = args.version
+    if version == None:
+        import configparser
+        c = configparser.ConfigParser()
+        c.read('config.txt')
+        version = c["Task Parameters"]["experiment_code_version"]
+        print("Fetching data for current version: ", version)
+
+    main(version, args.debug)
